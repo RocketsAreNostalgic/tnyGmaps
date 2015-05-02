9a6bc582aefc2b899a20ba70538d9bc4feb1c21f
orionrush <orionrush@gmail.com>
2015-05-02 18:19:59 +0100

    adding static image api for phones and small screens.

--- inc/js/tinygmaps.js
+++ inc/js/tinygmaps.js
@@ -1,41 +1,69 @@
 //http://codepen.io/anon/pen/zGxxaQ
-function initialize(map_id, map_loc) { 
-console.log("init"); 
-	var latlng = new google.maps.LatLng(map_loc.lat, map_loc.lng);
+
+function initialize(map_id, map_loc) {
+    console.log ('init');
+
+    var latlng = new google.maps.LatLng(map_loc.lat, map_loc.lng);
     var mapTypeId = map_loc.maptype;
-		var mapOptions = {
-		zoom: parseInt(map_loc.z),
+    var mapOptions = {
+        zoom: parseInt(map_loc.z),
         mapTypeId: google.maps.MapTypeId[mapTypeId],
-		center: latlng,
-		scrollwheel: map_loc.scrollwheel,
-		scaleControl: map_loc.scaleControl,
-		disableDefaultUI: map_loc.disableDefaultUI
-	};
-	map_id = new google.maps.Map(document.getElementById(map_id), mapOptions);
-	google.maps.event.addListenerOnce(map_id, 'tilesloaded', function() {
-		var marker = new google.maps.Marker({
-			map: map_id,
-			position: map_id.getCenter(),
-			icon: map_loc.marker
-		});
-		var infowindow = new google.maps.InfoWindow({
-			content: map_loc.infowindow,
-			position: latlng
-		});
-		infowindow.open(map_id, marker);
-        google.maps.event.addListener(marker, 'click', function() {
-            infowindow.open(map_id,marker);
+        center: latlng,
+        scrollwheel: map_loc.scrollwheel,
+        scaleControl: map_loc.scaleControl,
+        disableDefaultUI: map_loc.disableDefaultUI
+    };
+    // Start the map
+    map_id = new google.maps.Map(document.getElementById(map_id), mapOptions);
+
+    // Load the icon and bubble
+    google.maps.event.addListenerOnce(map_id, 'tilesloaded', function () {
+        var marker = new google.maps.Marker({
+            map: map_id,
+            position: map_id.getCenter(),
+            icon: map_loc.marker
         });
-	});
+        var infowindow = new google.maps.InfoWindow({
+            content: map_loc.infowindow,
+            position: latlng
+        });
+        infowindow.open(map_id, marker);
+        google.maps.event.addListener(marker, 'click', function () {
+            infowindow.open(map_id, marker);
+        });
+    });
+
+    // Centering on window resize
+    google.maps.event.addDomListener(window, "resize", function(){
+        var center = map_id.getCenter();
+        google.maps.event.trigger(map_id, "resize");
+        map_id.setCenter(center);
+    });
 }
 
+google.maps.event.addDomListener(window, "resize", function(map_id, map_loc){
+    if(document.documentElement.clientWidth > map_loc.static) {
+        jQuery('.tnygmps_canvas').each(function() {
+            var map_id = jQuery( this ).attr('id');
+            var map_loc = window[map_id + "_loc"]; // our localised scope
+            // Is the window large enough?
+            if(document.documentElement.clientWidth > map_loc.static) {
+                initialize(map_id, map_loc);
+            }
+        });
+    }
+});
+
+
+// Initialise on dom ready
 jQuery(document).ready(function(jQuery) {
     jQuery('.tnygmps_canvas').each(function() {
-    	var map_id = jQuery( this ).attr('id');
-		var map_loc = window[map_id + "_loc"]; // our localised scope
-		console.log(map_loc);
-		console.log(map_id);
-		initialize(map_id, map_loc);
-		});
+    var map_id = jQuery( this ).attr('id');
+    var map_loc = window[map_id + "_loc"]; // our localised scope
+        // Is the window large enough?
+        if(document.documentElement.clientWidth > map_loc.static) {
+            initialize(map_id, map_loc);
+        }
+    });
 });
 
