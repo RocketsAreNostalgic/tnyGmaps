aa3c4948129ba6471f6f70df70da2bbc4854bee3
orionrush <orionrush@gmail.com>
2015-05-04 15:29:19 +0100

    Added a check to make sure we weren't initialising a map on an already loaded map.

--- inc/js/tinygmaps.js
+++ inc/js/tinygmaps.js
@@ -1,37 +1,41 @@
 //http://codepen.io/anon/pen/zGxxaQ
 
 function initialize(map_id, map_loc) {
-    console.log ('init');
+    // an attribute added by google api after init has already been called
+    var attr = jQuery('#'+map_id).attr('jstcache');
+    // prevent init from being called on maps already drawn
+    if (typeof attr === typeof undefined || attr === false || attr === "" ) {
+        console.log('init');
 
-    var latlng = new google.maps.LatLng(map_loc.lat, map_loc.lng);
-    var mapTypeId = map_loc.maptype;
-    var mapOptions = {
-        zoom: parseInt(map_loc.z),
-        mapTypeId: google.maps.MapTypeId[mapTypeId],
-        center: latlng,
-        scrollwheel: map_loc.scrollwheel,
-        scaleControl: map_loc.scaleControl,
-        disableDefaultUI: map_loc.disableDefaultUI
-    };
-    // Start the map
-    map_id = new google.maps.Map(document.getElementById(map_id), mapOptions);
+        var latlng = new google.maps.LatLng(map_loc.lat, map_loc.lng);
+        var mapTypeId = map_loc.maptype;
+        var mapOptions = {
+            zoom: parseInt(map_loc.z),
+            mapTypeId: google.maps.MapTypeId[mapTypeId],
+            center: latlng,
+            scrollwheel: map_loc.scrollwheel,
+            scaleControl: map_loc.scaleControl,
+            disableDefaultUI: map_loc.disableDefaultUI
+        };
+        // Start the map
+        map_id = new google.maps.Map(document.getElementById(map_id), mapOptions);
 
-    // Load the icon and bubble
-    google.maps.event.addListenerOnce(map_id, 'tilesloaded', function () {
-        var marker = new google.maps.Marker({
-            map: map_id,
-            position: map_id.getCenter(),
-            icon: map_loc.marker
-        });
-        var infowindow = new google.maps.InfoWindow({
-            content: map_loc.infowindow,
-            position: latlng
-        });
-        infowindow.open(map_id, marker);
-        google.maps.event.addListener(marker, 'click', function () {
+        // Load the icon and bubble
+        google.maps.event.addListenerOnce(map_id, 'tilesloaded', function () {
+            var marker = new google.maps.Marker({
+                map: map_id,
+                position: map_id.getCenter(),
+                icon: map_loc.marker
+            });
+            var infowindow = new google.maps.InfoWindow({
+                content: map_loc.infowindow,
+                position: latlng
+            });
             infowindow.open(map_id, marker);
+            google.maps.event.addListener(marker, 'click', function () {
+                infowindow.open(map_id, marker);
+            });
         });
-    });
 
     // Centering on window resize
     google.maps.event.addDomListener(window, "resize", function(){
