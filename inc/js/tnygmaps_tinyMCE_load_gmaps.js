a3b1e1c92d689aa6c23e82f4f2dc623dca93e668
Ben Rush <orionrush@gmail.com>
2017-03-06 09:51:37 +0000

    - made tnygmaps_modal_loadmaps.js to tnygmaps_tinyMCE_load_gmapsn and modal_logic.js for more semantic file names.

--- inc/js/tnygmaps_tinyMCE_load_gmaps.js
+++ inc/js/tnygmaps_tinyMCE_load_gmaps.js
@@ -1,37 +1,37 @@
 (function () {
-        tinymce.create('tinymce.plugins.TNYGMAPS', {
-            init: function (ed, url) {
-                // Register commands
-                ed.addCommand('TNYGMAPS', function () {
-                    ed.windowManager.open({
-                        file: ajaxurl + '?action=tnygmaps_modal', // modal window
-                        width: 520 + parseInt(ed.getLang('button.delta_width', 0)), // size of our window
-                        height: 620 + parseInt(ed.getLang('button.delta_height', 0)),
-                        inline: 1
-                    }, {
-                        plugin_url: url
-                    });
-                });
-                // Register buttons
-                ed.addButton('TNYGMAPS', {
-                    title: 'Insert Google Map',
-                    image: url + '/../app_icon.png',
-                    cmd: 'TNYGMAPS'
-                });
-            },
-            getInfo: function () {
-                return {
-                    longname: 'TNYGMAPS',
-                    author: 'Ben Rush',
-                    authorurl: 'http://www.orionrush.com',
-                    infourl: 'http://www.orionrush.com',
-                    version: '1.0',
-                    version: tinymce.majorVersion + '.' + tinymce.minorVersion
-                };
-            }
-        });
-        // Register plugin
-        // first parameter is the button ID and must match ID elsewhere
-        // second parameter must match the first parameter of the tinymce.create() function above
-        tinymce.PluginManager.add('TNYGMAPS', tinymce.plugins.TNYGMAPS);
-    })();
+    tinymce.create('tinymce.plugins.TNYGMAPS', {
+        init: function (ed, url) {
+            // Register commands
+            ed.addCommand('TNYGMAPS', function () {
+                 ed.windowManager.open({
+                     url: ajaxurl + '?action=tnygmaps_modal', // modal window
+                     width: 520 + parseInt(ed.getLang('button.delta_width', 0)), // size of our window
+                     height: 620 + parseInt(ed.getLang('button.delta_height', 0)),
+                     inline: 1
+                 }, {
+                     plugin_url: url
+                 });
+            });
+            // Register buttons
+            ed.addButton('TNYGMAPS', {
+                title: 'Insert Google Map',
+                image: url + '/../app_icon.png',
+                cmd: 'TNYGMAPS'
+            });
+        },
+        getInfo: function () {
+            return {
+                longname: 'TNYGMAPS',
+                author: 'Ben Rush',
+                authorurl: 'http://www.orionrush.com',
+                infourl: 'http://www.orionrush.com',
+                version: '1.0',
+                version: tinymce.majorVersion + '.' + tinymce.minorVersion
+            };
+        }
+    });
+    // Register plugin
+    // first parameter is the button ID and must match ID elsewhere
+    // second parameter must match the first parameter of the tinymce.create() function above
+    tinymce.PluginManager.add('TNYGMAPS', tinymce.plugins.TNYGMAPS);
+})();
